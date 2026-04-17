ccd6f411980f7e4bea1f123bb967a0b2646704e4
Tathagata16 <tathagataghosh1609@gmail.com>
2026-04-17 21:12:12 +0530

    queue reconfigure

--- backend/src/queues/queue.js
+++ backend/src/queues/queue.js
@@ -1,7 +1,7 @@
 import { Queue } from 'bullmq';
 import Redis from 'ioredis';
 
-const connection = new Redis({ maxRetriesPerRequest: null });
+const connection = new Redis(process.env.REDIS_URL,{ maxRetriesPerRequest: null });
 
 // This is the queue "Producer"
 export const submissionQueue = new Queue('submission-queue', { connection });
